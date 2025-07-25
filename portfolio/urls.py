71e466b3c505c2d48589c506d3463743c5285ee2
Mssoares7 <matheusdasilvasoares@gmail.com>
2025-07-25 17:21:50 -0300

    Remove view de redefinição de superusuário por segurança

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import redefinir_senha_admin
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,7 +7,4 @@ urlpatterns = [
     path('curriculo-en/', views.curriculo_en, name='curriculo_en'),
     path('projetos/<int:pk>/', views.projeto_detail, name='projeto_detail'),
     path('publicacoes/', views.publicacoes, name='publicacoes'),
-    path('rodar-migracoes/', views.rodar_migracoes, name='rodar_migracoes'),
-    path('criar-admin/', views.cria_superusuario, name='criar_superusuario'),
-    path("redefinir-admin/", redefinir_senha_admin),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Projeto, Tecnologia, Publicacao
 from django.core.management import call_command
-from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
 
 
 def home(request):
@@ -27,40 +25,3 @@ def projeto_detail(request, pk):
 def publicacoes(request):
     publicacoes = Publicacao.objects.order_by('-data_publicacao')
     return render(request, 'portfolio/publicacoes.html', {'publicacoes': publicacoes})
-
-def rodar_migracoes(request):
-    call_command('migrate')
-    return HttpResponse("Migrações aplicadas com sucesso.")
-
-def cria_superusuario(request):
-    from django.contrib.auth.models import User
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "admin123")
-        return HttpResponse("Superusuário criado com sucesso.")
-    else:
-        return HttpResponse("Superusuário já existe.")
-
-def redefinir_senha_admin(request):
-    try:
-        user = User.objects.get(username="admin")
-        user.set_password("admin321")
-        user.save()
-        return HttpResponse("Senha redefinida com sucesso para 'admin321'.")
-    except User.DoesNotExist:
-        return HttpResponse("Usuário admin não encontrado.")
-
-def redefinir_senha_admin(request):
-    User = get_user_model()
-    username = "admin"
-    password = "admin321"
-
-    user, created = User.objects.get_or_create(username=username)
-    user.set_password(password)
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
-
-    if created:
-        return HttpResponse("Superusuário 'admin' criado com sucesso.")
-    else:
-        return HttpResponse("Senha redefinida para o usuário 'admin'.")
