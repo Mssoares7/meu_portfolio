197472b5b5e8a66177220f1bc3379b7b8001aeb1
Mssoares7 <matheusdasilvasoares@gmail.com>
2025-07-25 16:26:41 -0300

    Adiciona view temporária para criar superusuário

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from .views import cria_superusuario
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +9,5 @@ urlpatterns = [
     path('curriculo-en/', views.curriculo_en, name='curriculo_en'),
     path('projetos/<int:pk>/', views.projeto_detail, name='projeto_detail'),
     path('publicacoes/', views.publicacoes, name='publicacoes'),
+    path("criar-admin-secret-2782/", cria_superusuario),  # use um nome bem aleatório aqui!
 ]
