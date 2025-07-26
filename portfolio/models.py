d123530e187384efa43f3814296ba0768a1ac874
Mssoares7 <matheusdasilvasoares@gmail.com>
2025-07-26 11:02:04 -0300

    Corrige versão do Python para compatibilidade com Render3

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from cloudinary.models import CloudinaryField
 
 class Tecnologia(models.Model):
     nome = models.CharField(max_length=100)
@@ -12,7 +11,7 @@ class Projeto(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
     tecnologias = models.ManyToManyField(Tecnologia, blank=True)
-    imagem = models.ImageField(upload_to='projetos/', blank=True, null=True)
+    imagem = CloudinaryField('imagem', blank=True, null=True)  # alterado
     link_demo = models.URLField(blank=True)
     link_codigo = models.URLField(blank=True)
 
@@ -23,9 +22,8 @@ class Publicacao(models.Model):
     titulo = models.CharField(max_length=200)
     texto = models.TextField()
     data_publicacao = models.DateField(auto_now_add=True)
-    imagem = models.ImageField(upload_to='publicacoes/', blank=True, null=True)
+    imagem = CloudinaryField('imagem', blank=True, null=True)  # alterado
     publicado = models.BooleanField(default=True)
-
 
     def __str__(self):
         return self.titulo
